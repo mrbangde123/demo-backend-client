de8af3d27686b40b2b386418c4e648cce0a3507a
mrbangde123 <860217746@qq.com>
2026-05-10 15:39:34 +0800

    1234

--- src/main/java/com/hehui/client/config/CorsConfig.java
+++ src/main/java/com/hehui/client/config/CorsConfig.java
@@ -1,2 +1,18 @@
-package com.hehui.client.config;public class CorsConfig {
+package com.hehui.client.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
 }
